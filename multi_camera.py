1b242f04682ed8dd130bdc836357b03bd2347271
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2022-08-20 11:56:15 -0700

    added more class methods for a complete pipeline
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- multi_camera.py
+++ multi_camera.py
@@ -2,7 +2,7 @@
 # with the ability to output multiple cameras sequentially,
 # named in an organized manner.
 import bpy
-import print_console
+from print_console import print
 
 # get scene for top ref
 scene = bpy.context.scene
@@ -15,10 +15,36 @@ class CameraHandler:
     def __init__(self) -> None:
         pass
 
-    def get_camera_names():
+    """
+    set_consistent_cameras - sets consistent naming convention
+    for each camera in the scene, referencing the project name given.
+     """
+
+    def set_consistent_cameras(self):
+        print("not implemented")
+
+    """
+    set_camera_path - sets the path for each camera to render frames to. 
+    """
+
+    def set_camera_path(self, path: str):
+        # set the path for all the camera outputs.
+        self._render_path = bpy.path.abspath(path)
+        print(self._render_path)
+
+    def get_camera_names(self):
         cam_names = [cam.name for cam in cams]
         for name in cam_names:
-            print_console.print("camera: " + name)
+            print("camera: " + name)
+
+    def render_preview_pixel(self):
+        print("not implemented")
+
+    def render_final_pixel(self):
+        cam_ops = bpy.ops
+        ctx = bpy.context
 
 
-CameraHandler.get_camera_names()
+handler = CameraHandler()
+handler.set_camera_path("../../renders")
+handler.render_final_pixel()
