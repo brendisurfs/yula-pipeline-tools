280a522d56656622fe0621db12a384d56d24d13f
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2022-08-20 10:49:12 -0700

    moved custom print to blender path, updated fns to class
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- multi_camera.py
+++ multi_camera.py
@@ -1,9 +1,24 @@
-# this module attempts to mimic the houdini ROP network, 
+# this module attempts to mimic the houdini ROP network,
 # with the ability to output multiple cameras sequentially,
 # named in an organized manner.
 import bpy
+import print_console
+
+# get scene for top ref
+scene = bpy.context.scene
+
+# cameras
+cams = bpy.data.cameras
 
 
+class CameraHandler:
+    def __init__(self) -> None:
+        pass
+
+    def get_camera_names():
+        cam_names = [cam.name for cam in cams]
+        for name in cam_names:
+            print_console.print("camera: " + name)
 
 
-
+CameraHandler.get_camera_names()
