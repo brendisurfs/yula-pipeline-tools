a1643d92c437bf5375a8228737380ebb7a4aeb32
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2022-08-25 12:49:46 -0700

    added initial render settings for preview
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

---
+++ render-operators/OP_preview_render.py
@@ -0,0 +1,38 @@
+from typing import ContextManager
+import bpy
+from bpy.types import Context, Event, OperatorOptions
+
+# custom print script on load
+from print_console import print
+
+
+class YULA_OT_YTrender(bpy.types.Operator):
+    """Tooltip here"""
+
+    bl_idname: str = "yrt.render_preview"
+    bl_label: str = "Set Preview Render"
+
+    @classmethod
+    def poll(cls, context: Context):
+        return context.area.type == "VIEW_3D"
+
+    def execute(self, context: Context):
+        scene = context.scene
+        render_settings = scene.render
+        render_settings.fps = 30
+        render_settings.resolution_x = 640
+        render_settings.resolution_y = 480
+        print("rendering!")
+        return {"FINISHED"}
+
+
+def register():
+    bpy.utils.register_class(YULA_OT_YTrender)
+
+
+def unregister():
+    bpy.utils.unregister_class(YULA_OT_YTrender)
+
+
+if __name__ == "__main__":
+    register()
