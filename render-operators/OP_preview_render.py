ab6e094670481eacd66bc2236d78ab329cb62b8b
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2022-08-25 14:10:18 -0700

    adjusted min size
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- render-operators/OP_preview_render.py
+++ render-operators/OP_preview_render.py
@@ -14,6 +14,29 @@ class YULA_OT_YTrender(bpy.types.Operator):
 
     bl_idname: str = "yrt.render_preview"
     bl_label: str = "Set Preview Render"
+    # prop
+    res_x: bpy.props.IntProperty(
+        name="X Resolution",
+        description="resolution of X in pixels",
+        default=1920,
+        min=256,
+        soft_max=4096,
+    )
+    # prop
+    res_y: bpy.props.IntProperty(
+        name="Y Resolution",
+        description="resolution of Y in pixels",
+        default=1080,
+        min=256,
+        soft_max=4096,
+    )
+
+    # prop
+    send_email: bpy.props.BoolProperty(
+        name="Send Email",
+        description="send an email over smtp server once render is done",
+        default=False,
+    )
 
     @classmethod
     def poll(cls, context: Context):
@@ -26,11 +49,17 @@ class YULA_OT_YTrender(bpy.types.Operator):
     def execute(self, context: Context):
         render_settings = context.scene.render
 
+        # always 30fps
         render_settings.fps = 30
-        render_settings.resolution_x = 640
-        render_settings.resolution_y = 480
 
-        print("rendering!")
+        render_settings.resolution_x = self.res_x
+        render_settings.resolution_y = self.res_y
+
+        print(
+            "rendering info: {} {}".format(
+                render_settings.resolution_x, render_settings.resolution_y
+            )
+        )
         return {"FINISHED"}
 
 
--- ui-panels/ui_multicam.py
+++ ui-panels/ui_multicam.py
@@ -1,9 +1,11 @@
 import typing
 import bpy
-import bpy
 from bpy.types import Context
 
+# import Operators
 
+
+# MAIN UI
 class VIEW_3D_PT_yrt_panel(bpy.types.Panel):
     bl_space_type: typing.Union[str, int] = "VIEW_3D"
     bl_region_type: typing.Union[str, int] = "UI"
@@ -11,10 +13,22 @@ class VIEW_3D_PT_yrt_panel(bpy.types.Panel):
     bl_label: str = "Render"
 
     def draw(self, context: Context):
-        # add render buttons
-        self.layout.operator(
-            "yrt.render_preview", text="Render Preview", icon="RENDER_ANIMATION"
+
+        # render column
+        col = self.layout.column(align=True)
+
+        # RENDER BUTTON PROPS
+        props = col.operator(
+            "yrt.render_preview", text="Render Small Preview", icon="RENDER_ANIMATION"
         )
+        props.res_x = 640
+        props.res_y = 480
+
+        col = self.layout.column(align=True)
+        col.prop(context.scene.eevee, "taa_render_samples")
+        # SEND EMAIL PROP
+        # props = col.operator("yrt.render_preview", text="send email on finish?")
+        # props.send_email = col.activate_init
         pass
 
 
