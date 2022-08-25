718cf74aa231bab685e44cc84281e7d9d8a6eaf7
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2022-08-25 13:22:43 -0700

    added initial setup for UI elements
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- ui-panels/ui_multicam.py
+++ ui-panels/ui_multicam.py
@@ -1,60 +1,25 @@
-# NOTE: This doesnt work right now, only laying out for now.
 import typing
 import bpy
-
-from bpy.props import EnumProperty, PointerProperty
-from bpy.types import AnyType, Point
-
-from print_console import print
+import bpy
+from bpy.types import Context
 
 
-class MultiCam_PT_TemplateOperator(bpy.types.Operator):
-    bl_label: str = "Multicam operator"
-    bl_idname: str = "wm.multicam_operator"
-
-    preset_prop: bpy.props.EnumProperty(
-        name="render fns", description="select an option", items={"preview", "render"}
-    )
-
-    def execute(self, context):
-        print("my preset: ", self.preset_prop)
-        return {"Finished"}
-
-
-class MULTICAM_PT_layout_panel(bpy.types.Panel):
-    bl_label: str = "MultiCam"
-    bl_category: str = "Multi Cam"
+class VIEW_3D_PT_yrt_panel(bpy.types.Panel):
     bl_space_type: typing.Union[str, int] = "VIEW_3D"
     bl_region_type: typing.Union[str, int] = "UI"
+    bl_category: str = "Yula Render Tools"
+    bl_label: str = "Render"
 
-    def draw(self, context):
-        layout = self.layout
-        scene = context.scene
-
-        col = layout.column()
-        col.prop(scene, "camera_collection")
-
-        # button
-        col = layout.column()
-        col.prop(scene, "multicam_operator_preview")
+    def draw(self, context: Context):
+        pass
 
 
 def register():
-    scene_collection = bpy.types.Scene
-
-    camera_collection = PointerProperty(
-        name="Camera Collection", type=bpy.types.Collection
-    )
-
-    scene_collection.camera_collection = camera_collection
-    bpy.utils.register_class(MULTICAM_PT_layout_panel)
-
-    stored_camera_collection = bpy.context.scene.camera_collection
+    bpy.utils.register_class(VIEW_3D_PT_yrt_panel)
 
 
 def unregister():
-    bpy.utils.unregister_class(MULTICAM_PT_layout_panel)
-    del bpy.types.Scene.camera_collection
+    bpy.utils.unregister_class(VIEW_3D_PT_yrt_panel)
 
 
 if __name__ == "__main__":
